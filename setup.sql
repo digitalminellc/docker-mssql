d91e6a8950fe365ad3562bcc4d38ef391007bf34
stevewithington <steve@digitalmine.com>
2020-12-07 20:14:51 -0600

    update docker-compose and setup.sql

--- setup.sql
+++ setup.sql
@@ -4,7 +4,11 @@ Enter custom T-SQL here that would run after SQL Server has started up.
 
 */
 
-IF NOT EXISTS (SELECT * FROM sys.databases WHERE name = 'HelloWorld')
+IF NOT EXISTS (
+  SELECT NAME
+  FROM master.dbo.sysdatabases
+  WHERE NAME = N'HelloWorld'
+)
 BEGIN
   CREATE DATABASE HelloWorld;
 END;
